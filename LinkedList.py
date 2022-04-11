b308932ff21e5510c30c7b68e7c511d789fd558c
davient <dave@drdavient.com>
2022-04-11 17:07:12 +0100

    updates still looping

--- LinkedList.py
+++ LinkedList.py
@@ -5,60 +5,67 @@ class LinkedList:
     def __init__(self, name):
         self.head =None
         self.tail = None
-        
+
     def EnQueue(self, node):
-         if (self.head == None):
+        if (self.head == None):
             self.head = node
-         else:
-             self.tail.next = node
-         self.tail = node
-             
+        else:
+            self.tail.next = node
+        self.tail = node
+
     def Push(self, node):
         if (self.head == None):
             self.head = node
             self.tail = node
         else:
-             node.next = self.head
-             self.head = node
-             
-             
-    def Pop(self):
-         temp=self.head
-         self.head=self.head.next
-         return temp
-         
-     
-         
-class Node:
+            node.next = self.head
+            self.head = node
 
+    def Pop(self):
+        temp=self.head
+        self.head=temp.next
+        return temp
+
+    def Reverse(self):
+        self.PopTilEmpty()
+
+    def PopTilEmpty(self):
+        if(self.head):
+            current = self.Pop()
+            print(current.name + "Popped")
+            self.PopTilEmpty();
+            self.EnQueue(current);
+            print(current.name + "Queued")
+
+
+
+
+
+    def Print(self):
+        alphabet=""
+        current = self.head
+        while (current!=None):
+            alphabet += current.name
+            print(alphabet)
+            current=current.next
+            if (current):
+                alphabet += ","
+        print(alphabet)
+
+
+
+class Node:
     def __init__(self, name):
         self.name = name
-        self.next =None
-        
-    
-    
+        self.next = None
+
+
 
 if (__name__=="__main__"):
-    print("hello")
-    node1 =Node('A')
-    print (node1.name)
     letterList = LinkedList ("Letter List")
     for char in string.ascii_lowercase:
-        letterList.Push(Node(char))
-    
-    current = letterList.head
-    alphabet=""
-    reverse=""
-    while (current!=None):
-        alphabet +=current.name        
-        current=current.next
-        
-    print(alphabet)
-    reverse =""
-    while(letterList.head):
-         reverse=letterList.Pop().name+reverse
-         
-    print(reverse)
-    
-        
-    
+        letterList.EnQueue(Node(char))
+
+
+letterList.Reverse()
+letterList.Print()
